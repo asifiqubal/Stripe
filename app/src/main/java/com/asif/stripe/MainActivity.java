199a23598cb663fa683768551b6a5697322dd30a
Asif Iqubal <asif_iqubal@outlook.com>
2019-01-30 03:56:45 +0600

    Stripe Payment done

--- app/src/main/java/com/asif/stripe/MainActivity.java
+++ app/src/main/java/com/asif/stripe/MainActivity.java
@@ -1,8 +1,11 @@
 package com.asif.stripe;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,10 +13,16 @@ import android.widget.Toast;
 
 import com.stripe.android.TokenCallback;
 import com.stripe.android.model.Card;
-import com.stripe.android.Stripe;
+import com.stripe.Stripe;
 import com.stripe.android.model.Token;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
 
-import org.w3c.dom.Text;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
 
 public class MainActivity extends AppCompatActivity {
     //View attribute
@@ -21,10 +30,15 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextCardExpMonth;
     private EditText editTextCardExpYear;
     private EditText editTextCvc;
+    private EditText editTextCostoumerName;
+    private EditText editTextPhone;
+    private EditText editTextEmail;
+    private EditText editTextAmount;
     private Button buttonPay;
 
     //
     Context context;
+    public ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         context = this.getApplicationContext();
+
 
         initializeView();
         initializeClickListner();
@@ -42,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         buttonPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressDialog.show();
                 String cardNumber = editTextCardNumber.getText().toString();
                 int cardExpMonth = Integer.valueOf(editTextCardExpMonth.getText().toString());
                 int cardExpYear = Integer.valueOf(editTextCardExpYear.getText().toString());
@@ -52,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 if(!card.validateCard()){
                     Toast.makeText(context,"Your Card is not validate. \n Please Enter Valid Card Info",Toast.LENGTH_LONG).show();
                 }else {
-                    Stripe stripe = new Stripe(context,"pk_test_TYooMQauvdEDq54NiTphI7jx");
+                    com.stripe.android.Stripe stripe = new com.stripe.android.Stripe(context,"pk_test_TYooMQauvdEDq54NiTphI7jx");
                     stripe.createToken(card,
                             new TokenCallback() {
                                 @Override
@@ -63,7 +79,33 @@ public class MainActivity extends AppCompatActivity {
 
                                 @Override
                                 public void onSuccess(Token token) {
-                                    Toast.makeText(context,"Success :" +token.getId(),Toast.LENGTH_LONG).show();
+                                    Stripe.apiKey="sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+                                    int amount = (int) (Float.valueOf(editTextAmount.getText().toString())*100);
+                                    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                                    final Map<String,Object> params = new HashMap<>();
+                                    params.put("amount",amount);
+                                    params.put("currency", "usd");
+                                    params.put("description", "Test Charge");
+                                    params.put("source", token.getId());
+                                    Map<String, String> metadata = new HashMap<>();
+                                    metadata.put("order_id", "test_order_"+timestamp.getTime());
+                                    metadata.put("customer_name", editTextCostoumerName.getText().toString());
+                                    metadata.put("phone", editTextPhone.getText().toString());
+                                    metadata.put("email", editTextEmail.getText().toString());
+                                    metadata.put("receipt_email", editTextEmail.getText().toString());
+                                    params.put("metadata", metadata);
+
+                                    RequastToCharge requastToCharge = new RequastToCharge();
+                                    String crgid = null;
+                                    try {
+                                        crgid = requastToCharge.execute(params).get();
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    } catch (ExecutionException e) {
+                                        e.printStackTrace();
+                                    }
+                                    Toast.makeText(context,"Success :" +crgid,Toast.LENGTH_LONG).show();
+
                                 }
                             });
                 }
@@ -77,13 +119,44 @@ public class MainActivity extends AppCompatActivity {
         editTextCardExpMonth = (EditText) findViewById(R.id.editTextCardExpMonth);
         editTextCardExpYear = (EditText) findViewById(R.id.editTextCardExpYear);
         editTextCvc = (EditText) findViewById(R.id.editTextCvc);
+        editTextCostoumerName =(EditText) findViewById(R.id.editTextCustomerName);
+        editTextPhone = (EditText) findViewById(R.id.editTextPhone);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextAmount = (EditText) findViewById(R.id.editTextAmount);
         buttonPay = (Button) findViewById(R.id.buttonPay);
-    }
 
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Please Wait....");
+        progressDialog.setTitle("Stripe");
+    }
     @Override
     protected void onResume(){
         super.onResume();
+    }
 
+    private class RequastToCharge extends AsyncTask<Map<String, Object>, Void, String> {
 
+        @Override
+        protected String doInBackground(Map<String, Object>... maps) {
+            String result;
+            try {
+                Charge charge = Charge.create(maps[0]);
+                Log.i("crg_id",charge.getId());
+                result = charge.getId();
+                //Toast.makeText(context,"Success :" +charge.getId(),Toast.LENGTH_LONG).show();
+            } catch (StripeException e) {
+                e.printStackTrace();
+                result = e.getLocalizedMessage();
+                Log.e("Error Crg :",e.getLocalizedMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressDialog.dismiss();
+        }
     }
 }
